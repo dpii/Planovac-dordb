57a5b8c99cd0ef588a852c81d4f0e8bbcb9ac3df
dupal.petr@gmail.com <dupal.petr@gmail.com>
2013-03-26 05:35:52 +0100

    7_data v rukavu, jeste to zkontroluju behem dne
    todo: 6,8

--- sql/5_schema.sql
+++ sql/5_schema.sql
@@ -3,20 +3,49 @@ drop table skupiny_uzivatelu;
 drop table udalosti_uzivatelu;
 drop table pripominky;
 
-ALTER TABLE skupiny DROP CONSTRAINT fk_idVedouciho;
+ALTER TABLE skupina DROP CONSTRAINT fk_idVedouciho;
 
 alter table udalosti drop constraint fk_idVlastnikaSK;
 alter table udalosti drop constraint fk_idVlastnikaUz;
 
-drop table skupiny;
+drop table skupina;
 drop table uzivatele;
-drop table udalosti;
+drop table udalost;
+drop table adresa;
 
--- nezapomenout na triggery a sekvence - zustanou
+-- !! triggery a sekvence - zustanou
 
 
---drop table UZIVATELE;
-create table uzivatele (
+--------------------------------------------S
+--drop table adresa
+create table Adresa (
+  idAdresy number(4) CONSTRAINT nn_idAdresyA NOT NULL CONSTRAINT pk_idAdresyA PRIMARY KEY,
+  psc number(5),
+  ulice varchar2(40),
+  cisloPopisne number(4),
+  mesto varchar2(40),
+  poznamka varchar2(30)
+);
+
+--autoincrement Adresa.idAdresy
+CREATE SEQUENCE seq_adresa
+INCREMENT BY 1
+START WITH 1
+NOMAXVALUE
+MINVALUE 0;
+ 
+CREATE TRIGGER adresa
+BEFORE INSERT ON adresa
+FOR EACH ROW
+begin
+  select seq_adresa.nextval into :new.idAdresy from dual;
+end;
+--------------------------------------------E
+
+-------------------------------------------S
+
+--drop table UZIVATEL;
+create table uzivatel (
   idUzivatele number(10) CONSTRAINT pk_idUzivatele primary key CONSTRAINT nn_idUzivatele not null,
   email varchar2(40) CONSTRAINT nn_email NOT NULL,
   jmeno varchar2(30),
@@ -26,9 +55,9 @@ create table uzivatele (
   role varchar2(45) CONSTRAINT nn_role NOT NULL,
   datumZalozeni DATE,
   datumPosledniPrihlaseni DATE,
-  adresa varchar2(30),
-  mesto varchar2(30),
-  telefon varchar2(20)
+  telefon varchar2(20),
+  idAdresy number(4),
+  CONSTRAINT fk_idAdresy FOREIGN KEY (idAdresy) REFERENCES Adresa(idAdresy)
 );
 
 
@@ -45,123 +74,125 @@ alter table uzivatele modify (
 --insert into uzivatele (login, email, heslo_hash, povolen) VALUES ('asd', 'asd@asd.com', '123', '1');
 
 
---autoincrement uzivatele.idUzivatele
-CREATE SEQUENCE seq_uzivatele
+--autoincrement uzivatel.idUzivatele
+CREATE SEQUENCE seq_uzivatel
 INCREMENT BY 1
 START WITH 1
 NOMAXVALUE
 MINVALUE 0;
  
-CREATE TRIGGER uzivatele
-BEFORE INSERT ON uzivatele
+CREATE TRIGGER uzivatel
+BEFORE INSERT ON uzivatel
 FOR EACH ROW
 begin
-  select seq_uzivatele.nextval into :new.idUzivatele from dual;
+  select seq_uzivatel.nextval into :new.idUzivatele from dual;
 end;
 
+-------------------------------------------E
+
+
+-------------------------------------------S
 
 --drop table skupiny;
-CREATE TABLE SKUPINY (
+CREATE TABLE Skupina (
   idSkupiny number(4) CONSTRAINT nn_idSkupiny NOT NULL CONSTRAINT pk_idSkupiny PRIMARY KEY,
   nazev varchar2(30),
+  hesloProPridani varchar2(30),
   idVedouciho number(4) CONSTRAINT nn_idVedouciho  NOT NULL,
   popis varchar2(255),
-  verejna number(1),
-  CONSTRAINT fk_idVedouciho FOREIGN KEY (idVedouciho) REFERENCES uzivatele(idUzivatele)
+  CONSTRAINT fk_idVedouciho FOREIGN KEY (idVedouciho) REFERENCES uzivatel(idUzivatele)
 );
 
 --autoincrement skupiny.idSkupiny
-CREATE SEQUENCE seq_skupiny
+CREATE SEQUENCE seq_skupina
 INCREMENT BY 1
 START WITH 1
 NOMAXVALUE
 MINVALUE 0;
  
-CREATE TRIGGER skupiny
-BEFORE INSERT ON skupiny
+CREATE TRIGGER skupina
+BEFORE INSERT ON Skupina
 FOR EACH ROW
 begin
-  select seq_skupiny.nextval into :new.idSkupiny from dual;
+  select seq_skupina.nextval into :new.idSkupiny from dual;
 end;
 
+-------------------------------------------E
+
+-------------------------------------------S
 --drop table udalosti;
-CREATE TABLE  UDALOSTI (
+CREATE TABLE  UDALOST (
   idUdalosti number(4) CONSTRAINT nn_idUdalosti NOT NULL CONSTRAINT pk_idUdalosti PRIMARY KEY,
   nazev varchar2(30),
-  verejna number(1),
   idVlastnikaSk number(4) ,
   idVlastnikaUz number(4) ,
-  obrazek varchar2(30),
   zacatek date,
   konec date,
+  verejna number(1),
+  idMistaKonani number(10),
+  
   popis varchar2(255),
-  CONSTRAINT fk_idVlastnikaSK foreign key (idVlastnikaSk) REFERENCES skupiny(idSkupiny),
-  CONSTRAINT fk_idVlastnikaUz foreign key (idVlastnikaUz) REFERENCES uzivatele(idUzivatele)
+  CONSTRAINT fk_idVlastnikaSK foreign key (idVlastnikaSk) REFERENCES skupina(idSkupiny),
+  CONSTRAINT fk_idVlastnikaUz foreign key (idVlastnikaUz) REFERENCES uzivatel(idUzivatele),
+  CONSTRAINT fk_idMistaKonani foreign key (idMistaKonani) REFERENCES Adresa(idAdresy)
 );
 
---autoincrement udalosti.idUdalosti
-CREATE SEQUENCE seq_udalosti
+
+--autoincrement udalost.idUdalosti
+CREATE SEQUENCE seq_udalost
 INCREMENT BY 1
 START WITH 1
 NOMAXVALUE
 MINVALUE 0;
  
-CREATE TRIGGER udalosti
-BEFORE INSERT ON udalosti
+CREATE TRIGGER udalost
+BEFORE INSERT ON udalost
 FOR EACH ROW
 begin
-  select seq_udalosti.nextval into :new.idUdalosti from dual;
+  select seq_udalost.nextval into :new.idUdalosti from dual;
 end;
+-------------------------------------------E
 
+-------------------------------------------S
 --drop table skupiny_uzivatelu;
 CREATE TABLE SKUPINY_UZIVATELU (
   idSkupiny number(4) CONSTRAINT nn_idSkupinyUz NOT NULL,
    idUzivatele number(4) CONSTRAINT nn_idUzivateleSk NOT NULL,
-   CONSTRAINT fk_idskupiny FOREIGN KEY (idSkupiny) REFERENCES SKUPINY(idSkupiny),
-  CONSTRAINT fk_idUzivatele FOREIGN KEY (idUzivatele) REFERENCES UZIVATELE(idUzivatele),
+   CONSTRAINT fk_idskupiny FOREIGN KEY (idSkupiny) REFERENCES SKUPINA(idSkupiny),
+  CONSTRAINT fk_idUzivatele FOREIGN KEY (idUzivatele) REFERENCES UZIVATEL(idUzivatele),
   CONSTRAINT uc_SkupinyUzivatele UNIQUE (idSkupiny,idUzivatele)
 );
+-------------------------------------------E
 
+-------------------------------------------S
 --drop table udalosti_uzivatelu;
 CREATE TABLE UDALOSTI_UZIVATELU (
   idUdalosti number(4)  CONSTRAINT nn_idUdalostiUU NOT NULL,
   idUzivatele number(4) CONSTRAINT nn_idUzivateleUU NOT NULL,
-  CONSTRAINT fk_UdalostiUU FOREIGN KEY (idUdalosti) REFERENCES UDALOSTI(idUdalosti),
-  CONSTRAINT fk_UzivateleUU FOREIGN KEY (idUzivatele) REFERENCES UZIVATELE(idUzivatele),
+  typ_pripominky number(4) ,
+  casUpominky DATE,
+  intervalOpakovani number(4) ,
+  aktivni number(1),
+  CONSTRAINT fk_UdalostiUU FOREIGN KEY (idUdalosti) REFERENCES UDALOST(idUdalosti),
+  CONSTRAINT fk_UzivateleUU FOREIGN KEY (idUzivatele) REFERENCES UZIVATEL(idUzivatele),
   CONSTRAINT uc_UdalUziv UNIQUE (idUdalosti,idUzivatele)
 );
-
---drop table pripominky
-CREATE TABLE PRIPOMINKY (
-  idPripominky number(4) CONSTRAINT nn_idPripominky NOT NULL CONSTRAINT pk_idPripominky PRIMARY KEY,
-  idUdalosti number(4) CONSTRAINT nn_idUdalostiP NOT NULL,
-  idUzivatele number(4) CONSTRAINT nn_idUdzivateleP NOT NULL,
-  typ_pripominky number(4) ,
-  cas_pred_udalosti DATE,
-  opakovani_po_intervalu number(4) ,
-  aktivni number(1),
-  CONSTRAINT fk_idUdalostiP FOREIGN KEY (idUdalosti) REFERENCES UDALOSTI(idUdalosti),
-  CONSTRAINT fk_idUzivateleP FOREIGN KEY (idUzivatele) REFERENCES UZIVATELE(idUzivatele)
-);
-
---autoincrement pripominky.idPripominky
-CREATE SEQUENCE seq_pripominky
-INCREMENT BY 1
-START WITH 1
-NOMAXVALUE
-MINVALUE 0;
- 
-CREATE TRIGGER pripominky
-BEFORE INSERT ON pripominky
-FOR EACH ROW
-begin
-  select seq_pripominky.nextval into :new.idPripominky from dual;
-end;
+--------------------------------------------E
 
 
-grant all on uzivatele to nachato1;
-grant all on skupiny to nachato1;
-grant all on udalosti to nachato1;
+
+-- tomatova nachazelofka
+
+grant all on uzivatel to nachato1;
+grant all on skupina to nachato1;
+grant all on udalost to nachato1;
 grant all on skupiny_uzivatelu to nachato1;
 grant all on udalosti_uzivatelu to nachato1;
-grant all on pripominky to nachato1;
+grant all on adresa to nachato1;
+
+revoke all on uzivatel from nachato1;
+revoke all on skupina from nachato1;
+revoke all on udalost from nachato1;
+revoke all on skupiny_uzivatelu from nachato1;
+revoke all on udalosti_uzivatelu from nachato1;
+revoke all on adresa from nachato1;
