492bdec2280373975eb32cf069fbea7a8acd3552
dupal.petr@gmail.com <dupal.petr@gmail.com>
2013-03-30 02:55:09 +0100

    12_ mutation error :P treba doladit

--- sql/12_deriv_column.sql
+++ sql/12_deriv_column.sql
@@ -1,4 +1,19 @@
 -- Pro každého uživatele ukáže, kolik času zbývá do nejblížší události
 
-select u.idUdalosti, u.nazev, u.idVlastnika, u.idskupiny,
-		u.zacatek-to_date(sysdate, 'DD.MM.YYYY HH24:MI:SS') as zacina_za_x_dni_od_vlozeni from udalost u, dual;
+create or replace procedure vypocti_dobu(ide in number, zacat in date) as
+pocet number;
+begin
+
+pocet := zacat - to_date(sysdate, 'DD.MM.YYYY HH24:MI:SS');
+ 
+update udalosti_uzivatelu set od_vlozeni_zacne_za = pocet where idUdalosti = ide;
+ 
+end;
+
+create or replace trigger vypocet_doby
+after insert or update
+ON udalost
+for each row  
+BEGIN
+  vypocti_dobu(:new.idUdalosti,:new.zacatek);
+END;
