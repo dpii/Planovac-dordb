3adbf2ca5e9cd99291254b0235c643fc4841ab7e
dupal.petr@gmail.com <dupal.petr@gmail.com>
2013-05-11 00:42:19 +0200

    load obj_uzivatel

--- sql/13_load_objects.sql
+++ sql/13_load_objects.sql
@@ -1,6 +1,6 @@
 insert into obj_mesto select * from mesto;
 
-
+--------------
 
 insert into obj_adresa
 select
@@ -11,3 +11,25 @@ select
   ,mesto
   poznamka
 from adresa ad;
+
+
+-----------
+
+insert into obj_uzivatel
+select
+   iduzivatele
+  ,email
+  ,jmeno
+  ,prijmeni
+  ,heslo
+  ,povolen
+  ,role
+  ,datumzalozeni
+  ,datumposledniprihlaseni
+  ,telefon
+  ,(select ref(obj) from obj_adresa obj where obj.idAdresy = uz.idAdresy)
+from uzivatel uz;
+
+
+-----------
+
